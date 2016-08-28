2f6bff1be1f4cb0fd5d4dab2b798fa7226d15f80
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-08-28 14:20:28 -0300

    Separação do código em Javascript em um arquivo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+$(document).ready(function() {
+	$('#search-query').focus();
+});
 
+$('#advanced-search-button').click(function() {
+	$('.advanced-search-panel').slideToggle('fast');
+	$(this).toggleClass('active');
+});

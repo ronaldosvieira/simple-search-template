98296a5391c0e7a79a88e413e4d781fd1b7139a8
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-09-04 20:31:59 -0300

    Primeira versão da página de resultados

--- js/main.js
+++ js/main.js
@@ -6,3 +6,7 @@ $('#advanced-search-button').click(function() {
 	$('.advanced-search-panel').slideToggle('fast');
 	$(this).toggleClass('active');
 });
+
+$(document).ready(function ($) {
+    $('#tabs').tab();
+});
